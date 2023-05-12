b73a081359e400ba3de75ac0e60f7b571bd6ccf3
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2023-05-12 20:40:04 -0300

    Update project

--- src-trabalho4/br/unicamp/ic/inf335/beans/AnuncioBeanTest.java
+++ src-trabalho4/br/unicamp/ic/inf335/beans/AnuncioBeanTest.java
@@ -1,3 +1,5 @@
+package br.unicamp.ic.inf335.beans;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
--- src-trabalho4/br/unicamp/ic/inf335/beans/ProdutoBeanTest.java
+++ src-trabalho4/br/unicamp/ic/inf335/beans/ProdutoBeanTest.java
@@ -1,3 +1,5 @@
+package br.unicamp.ic.inf335.beans;
+
 //import org.junit.Test;
 import org.junit.jupiter.api.Test;
 //import static junit.framework.TestCase.assertEquals;
